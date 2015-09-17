8d8844042c0a01acf0e7e82fb4570717de57e9d8
Adenike Adebiyi <adenikeadebiyi@icloud.com>
2015-09-16 23:39:05 -0500

    Checkpoint 31 completed, crud intro

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'posts/index'
 
-  get 'welcome/about'
+  get 'posts/show'
+
+  get 'posts/new'
+
+  get 'posts/edit'
+
+  # #1
+  resources :posts
+
+  # Allows users to type /about rather than /welcome/about
+  get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
 end
