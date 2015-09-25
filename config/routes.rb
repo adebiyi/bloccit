006d4c7c9db303aa600ae552a425ca9b461a8d88
Adenike Adebiyi <adenikeadebiyi@icloud.com>
2015-09-25 18:58:16 -0500

    Checkpoint 37 completed, Topics adn Posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-
-  get 'posts/show'
-
-  get 'posts/new'
-
-  get 'posts/edit'
 
   # #1
-  resources :posts
+  resources :topics do
+    resources :posts, except: [:index]
+  end
 
   # Allows users to type /about rather than /welcome/about
   get 'about' => 'welcome#about'
