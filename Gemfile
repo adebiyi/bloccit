bc921f05c5cd22bf06fdbea06f2a08154e7fb60d
Adenike Adebiyi <adenikeadebiyi@icloud.com>
2015-09-27 22:57:46 -0500

    Checkpoint 39 completed, authentication

--- Gemfile
+++ Gemfile
@@ -38,3 +38,5 @@ gem 'turbolinks'
 
 #Use Bootstrap
 gem 'bootstrap-sass'
+
+gem 'bcrypt'
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
   end
 
+  resources :users, only: [:new, :create]
+
   # Allows users to type /about rather than /welcome/about
   get 'about' => 'welcome#about'
 
