b91691208c60795065cad9a8ef5833f23497caf7
Adenike Adebiyi <adenikeadebiyi@icloud.com>
2015-10-15 22:32:12 -0500

    Checkpoint 39 completed, posts and users

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -24,9 +24,10 @@ RSpec.describe UsersController, type: :controller do
   end
 
   describe "POST create" do
-    it "returns http success" do
+
+    it "returns http redirect" do
       post :create, user: new_user_attributes
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:redirect)
     end
 
     it "creates a new user" do
@@ -37,27 +38,27 @@ RSpec.describe UsersController, type: :controller do
 
     it "sets user name properly" do
       post :create, user: new_user_attributes
-      expect(assigns(:user).name).to new_user_attributes[:name]
+      expect(assigns(:user).name).to eq new_user_attributes[:name]
     end
 
     it "sets email properly" do
       post :create, user: new_user_attributes
-      expect(assigns(:user).email).to new_user_attributes[:email]
+      expect(assigns(:user).email).to eq new_user_attributes[:email]
     end
 
     it "sets user password properly" do
       post :create, user: new_user_attributes
-      expect(assigns(:user).password).to new_user_attributes[:password]
+      expect(assigns(:user).password).to eq new_user_attributes[:password]
     end
 
     it "sets user password_confirmation properly" do
       post :create, user: new_user_attributes
-      expect(assigns(:user).password_confirmation).to new_user_attributes[:password_confirmation]
+      expect(assigns(:user).password_confirmation).to eq new_user_attributes[:password_confirmation]
     end
 
     it "logs the user in after sign up" do
       post :create, user: new_user_attributes
-      expect(session[:user_id]).to eq assigns(:user).user_id
+      expect(session[:user_id]).to eq assigns(:user).id
     end
   end
 
