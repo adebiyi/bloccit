b51a5d33dbcf1f44b35357fd55993e89cc1356c1
Adenike Adebiyi <adenikeadebiyi@icloud.com>
2015-10-22 18:57:03 -0500

    checkpoint-40-authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,11 @@ class User < ActiveRecord::Base
   has_many :posts
   
   before_save { self.email = email.downcase }
+  before_save { self.role ||= :member }
 
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-
-  validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
-
-  validates :password, length: { minimum: 6 }, allow_blank: true
 
   validates :email,
             presence: true,
@@ -17,5 +14,11 @@ class User < ActiveRecord::Base
             length: { minimum: 3, maximum: 100 },
             format: { with: EMAIL_REGEX }
 
+  validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
+
+  validates :password, length: { minimum: 6 }, allow_blank: true
+
   has_secure_password
+
+  enum role: [:member, :admin]
 end
