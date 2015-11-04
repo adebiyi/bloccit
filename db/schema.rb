6f6daf0617f621e4900d5aeab7bf0d8d69845fec
Adenike Adebiyi <adenikeadebiyi@icloud.com>
2015-11-03 22:30:43 -0600

    Progress on checkpoint 43

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151028205511) do
+ActiveRecord::Schema.define(version: 20151104035820) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -77,6 +77,20 @@ ActiveRecord::Schema.define(version: 20151028205511) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "ratings", force: :cascade do |t|
+    t.integer  "severity",      default: 0
+    t.integer  "rateable_id"
+    t.string   "rateable_type"
+    t.integer  "topic_id"
+    t.integer  "post_id"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  add_index "ratings", ["post_id"], name: "index_ratings_on_post_id"
+  add_index "ratings", ["rateable_type", "rateable_id"], name: "index_ratings_on_rateable_type_and_rateable_id"
+  add_index "ratings", ["topic_id"], name: "index_ratings_on_topic_id"
+
   create_table "sponsored_posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
@@ -104,5 +118,16 @@ ActiveRecord::Schema.define(version: 20151028205511) do
     t.datetime "updated_at",      null: false
     t.integer  "role"
   end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
